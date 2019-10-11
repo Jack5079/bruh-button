c8ebf8e8fc185c7ac893cf6f7964965a174b7120
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-11 19:54:54 -0400

    some changes to the gui

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /* global localStorage, Audio */
+HTMLElement.prototype.click = function ( funct ) {
+  this.addEventListener( 'click', funct )
+}
 var bruh = 0
-if (localStorage.getItem('bruh')) bruh = JSON.parse(localStorage.getItem('bruh'))
-document.getElementById('bruh').onclick = () => {
-  var a = new Audio('bruh.mp3')
+if ( localStorage.getItem( 'bruh' ) ) bruh = JSON.parse( localStorage.getItem( 'bruh' ) )
+document.getElementById( 'bruh' ).click( () => {
+  var a = new Audio( 'bruh.mp3' )
   a.play()
   bruh++
-  localStorage.setItem('bruh', bruh)
-  document.getElementById('count').innerText = bruh
-}
+  localStorage.setItem( 'bruh', bruh )
+  document.getElementById( 'count' ).innerText = bruh
+} )
 // Load the bruh count
-document.getElementById('count').innerText = bruh
+document.getElementById( 'count' ).innerText = bruh
